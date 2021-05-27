e48eb3d3f33b82065828c43425f4282d7363f847
oeawfebe <oeawfebe@tutanota.com>
2021-05-27 16:37:15 -0400

    auto

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,10 +5,14 @@ contract Greeter {
     constructor() public {
         _owner=msg.sender;
     }
+    modifier onlyOwner() {
+        require(msg.sender==_owner,"Ownable:caller is not the owner");
+        _;
+    }
     function greet() external view returns(string memory) {
         return _greeting;
     }
-    function setGreeting(string calldata greeting) external {
+    function setGreeting(string calldata greeting) external onlyOwner {
         _greeting=greeting;
     }
     function owner() public view returns(address) {
